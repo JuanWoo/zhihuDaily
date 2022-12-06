f43dacfc726e51428413c79eaf0d69796fe0def0
JuanWoo <juanwoo49@gmail.com>
2022-12-06 11:20:14 +0800

    修复windows环境下，主目录路径带空格无法启动的问题

--- start.bat
+++ start.bat
@@ -25,7 +25,7 @@ rem set "JAVA_OPTS=-Xms256m -Xmx1024m -Dundertow.port=80 -Dundertow.host=0.0.0.0
 rem set "JAVA_OPTS=-Dundertow.port=80 -Dundertow.host=0.0.0.0"
 
 set APP_BASE_PATH=%~dp0
-set CP=%APP_BASE_PATH%config;%APP_BASE_PATH%lib\*
+set CP="%APP_BASE_PATH%config";"%APP_BASE_PATH%lib\*"
 java -Xverify:none %JAVA_OPTS% -cp %CP% %MAIN_CLASS%
 
 
